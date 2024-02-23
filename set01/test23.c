32cef8830b25f53d5da3704cb25063e14cff5b7f
chandanareddyyyy <reddyofficial.111@gmail.com>
2024-02-23 15:03:30 +0530

    committed 2/23/2024, 3:03:29 PM

--- set01/test23.c
+++ set01/test23.c
@@ -19,11 +19,11 @@ int stringcompare(char *string1, char *string2)
 {
     int i=0;
     int n=0;
-    for(i=0; string1[i]==string2[i] && string1[i]!='\0' && string2[i]!='\0'; i++ )
-    {
+    for(i=0; string1[i]==string2[i] && string1[i]!='\0' && string2[i]!='\0'; i++ );
+    
          n= string1[i]-string2[i];
          return n;
-    }
+    
     
 }
 void output(char *string1, char *string2, int result)
