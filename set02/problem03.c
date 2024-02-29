c175059c1fabb2408f450b9b8416e6fbc2b38fe6
chandanareddyyyy <reddyofficial.111@gmail.com>
2024-02-29 09:33:17 +0530

    committed 2/29/2024, 9:33:16 AM

--- set02/problem03.c
+++ set02/problem03.c
@@ -12,11 +12,11 @@ int input_number()
 }
 int is_composite(int n)
 {
-    if (n<=2)
+    if (n<=1)
     {
         return 0;
     }
-    for(int i=2;i>100;i++)
+    for(int i=2;i<n/2;i++)
     {
         if(n%i==0){
             return 1;
