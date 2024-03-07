e6534453cd6fd8d69660736df17d6a9a4411135e
chandanareddyyyy <reddyofficial.111@gmail.com>
2024-03-07 13:12:01 +0530

    committed 3/7/2024, 1:12:00 PM

--- set02/problem05.c
+++ set02/problem05.c
@@ -12,10 +12,34 @@ int input()
 }
 int find_gcd(int a, int b)
 {
-
+    int x;
+    if(a>b)
+    {
+         x=a;
+    }
+    else{ x=b;
+    }
+    for(int i=x ;i=0;i--)
+    {
+           if(a%i==0&&b%i==0)
+           {
+             return i;
+             
+           }
+        
+    }
+    
 
 }
 void output(int a, int b, int gcd)
 {
     printf("the GCD of %d and %d is %d",a,b,gcd);
+}
+int main()
+{
+    int a,b,gcd;
+   a = input();
+   b= input();
+   gcd=find_gcd( a, b);
+   output( a, b, gcd);
 }
---
+++ set02/problem06.c
@@ -0,0 +1,19 @@
+//06. Write a program to reverse a string.
+
+#include<stdio.h>
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+void input_string(char *a)
+{
+    printf("Enter the string you want to reverse");
+    scanf("%s",a);
+}
+void str_reverse(char *str, char *rev_str)
+{
+ 
+}
+void output(char *a, char *reverse_a)
+{
+    printf("The sting %c when reversed is %c");
+}
