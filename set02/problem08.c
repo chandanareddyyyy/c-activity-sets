28acfa3c487f02b1c44966ff1523023fb1a48660
chandanareddyyyy <reddyofficial.111@gmail.com>
2024-03-15 10:01:29 +0530

    committed 3/15/2024, 10:01:28 AM

--- set02/problem08.c
+++ set02/problem08.c
@@ -1,14 +1,14 @@
 // 08. Write a program to find the triangle with smallest area in n given triangles.
 #include<stdio.h>
 typedef struct _triangle {
-	float base, altitude, area, n_area;;
+	float base, altitude, area;
 } Triangle;
 int input_n();
 
 Triangle input_triangle();
 void input_n_triangles(int n, Triangle t[n]);
 void find_area(Triangle *t);
-void find_n_areas(int n, Triangle t[n],Triangle n_area);
+void find_n_areas(int n, Triangle t[n]);
 Triangle find_smallest_triangle(int n, Triangle t[n]);
 void output(int n, Triangle t[n], Triangle smallest);
 Triangle input_triangle()
@@ -29,7 +29,7 @@ void find_area(Triangle *t)
 {
 	t->area=(0.5*(t->base*t->altitude));
 }
-void find_n_areas(int n, Triangle t[n], Triangle n_area)
+void find_n_areas(int n, Triangle t[n])
 {
        
 	   for (int i = 0; i < n-1; i++) {
