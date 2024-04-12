66896c1a61c2a57cc33f32301351d2ea69593358
chandanareddyyyy <reddyofficial.111@gmail.com>
2024-04-12 09:24:10 +0530

    committed 4/12/2024, 9:24:09 AM

--- set02/problem08.c
+++ set02/problem08.c
@@ -6,7 +6,7 @@ typedef struct _triangle {
 int input_n();
 
 Triangle input_triangle();
-void input_n_triangles(int n, Triangle t[n]);
+void 
 void find_area(Triangle *t);
 void find_n_areas(int n, Triangle t[n]);
 Triangle find_smallest_triangle(int n, Triangle t[n]);
